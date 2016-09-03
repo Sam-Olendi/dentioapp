69d5aec89499087374f0ab7529915622beba25fd
Samantha Olendi <sam.olendi@gmail.com>
2016-09-03 19:23:31 +0300

    Set up SMTP

---
+++ server/mail.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+    process.env.MAIL_URL = 'smtp://postmaster%40appb2de3d2bb391466fa5b5408c873a6e80.mailgun.org:9c248b860e91cc24c639abec836e8338@smtp.mailgun.org:587';
+});
