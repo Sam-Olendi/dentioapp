a15952ad1acc916a18f6dd2ad33f48b2f7e95cc5
Samantha Olendi <sam.olendi@gmail.com>
2016-09-03 21:28:16 +0300

    Configure SMTP and mailgun

--- server/mail.js
+++ server/mail.js
@@ -1,3 +1,5 @@
 Meteor.startup(function () {
-    process.env.MAIL_URL = 'smtp://postmaster%40appb2de3d2bb391466fa5b5408c873a6e80.mailgun.org:9c248b860e91cc24c639abec836e8338@smtp.mailgun.org:587';
+    process.env.MAIL_URL = 'smtp://postmaster%40dentioapp.herokuapp.com:ef7d769e1e9df49e4eb8604b8c8fc741@smtp.mailgun.org:587';
+
+    Accounts.emailTemplates.siteName = 'Dentio for Gentle Dental Care Clinic';
 });
