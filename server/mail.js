c9c6f8e9f643a26f08eda768fb4fac4b36d1d975
Samantha Olendi <sam.olendi@gmail.com>
2016-09-08 07:25:20 +0300

    Configure process env

--- server/mail.js
+++ server/mail.js
@@ -1,4 +1,6 @@
 Meteor.startup(function () {
+    process.env['MAILGUN_API_KEY'] = 'key-7a6d340d25c1aa56d5f0dfd7a178cdf8';
+
     process.env.MAIL_URL = 'smtp://postmaster%40dentioapp.herokuapp.com:ef7d769e1e9df49e4eb8604b8c8fc741@smtp.mailgun.org:587';
 
     Accounts.emailTemplates.siteName = 'Dentio for Gentle Dental Care Clinic';
