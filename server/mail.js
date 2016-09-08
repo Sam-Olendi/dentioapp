6286f48e387b925d10bcc3abbaf1e940776843cf
Samantha Olendi <sam.olendi@gmail.com>
2016-09-08 07:29:55 +0300

    Configure process env

--- server/mail.js
+++ server/mail.js
@@ -1,5 +1,5 @@
 Meteor.startup(function () {
-    process.env['MAILGUN_API_KEY'] = 'key-7a6d340d25c1aa56d5f0dfd7a178cdf8';
+    process.env.MAILGUN_API_KEY = 'key-7a6d340d25c1aa56d5f0dfd7a178cdf8';
 
     process.env.MAIL_URL = 'smtp://postmaster%40dentioapp.herokuapp.com:ef7d769e1e9df49e4eb8604b8c8fc741@smtp.mailgun.org:587';
 
