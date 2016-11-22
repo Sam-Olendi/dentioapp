120ce033a4f2d60cc15ebe66ecde8282c61196a3
Sam-Olendi <sam.olendi@gmail.com>
2016-11-22 18:39:25 +0300

    Set foundations for the invoice reports

--- client/reports/reports.js
+++ client/reports/reports.js
@@ -1,9 +1,46 @@
-Template.reportsContent.onCreated(function () {
+Template.reportsContent.events({
+    'click .reports-tabs': function () {
+        /*
+         Tab functionality
+         More details: https://codyhouse.co/gem/responsive-tabbed-navigation/
+         */
+
+        var tabs = $('.body-flesh');
+        tabs.each(function () {
+            var tab = $(this),
+                tabItems = tab.find('.reports-tabs'),
+                tabContentWrapper = tab.children('.reports-tabs-content');
+
+
+            tab.on('click', '.reports-tabs-link', function (event) {
+                event.preventDefault();
+                var selectedItem = $(this);
+
+                if ( !selectedItem.hasClass('reports-tabs-link-is-selected') ) {
+                    var selectedTab = selectedItem.data('content'),
+                        selectedContent = tabContentWrapper.find('li[data-content="'+ selectedTab +'"]'),
+                        selectedContentHeight = selectedContent.innerHeight();
+
+                    tabItems.find('.reports-tabs-link-is-selected').removeClass('reports-tabs-link-is-selected');
+                    selectedItem.addClass('reports-tabs-link-is-selected');
+                    selectedContent.addClass('reports-tabs-content-section-is-selected').siblings('li').removeClass('reports-tabs-content-section-is-selected');
+                    tabContentWrapper.animate({
+                        'height': selectedContentHeight
+                    }, 200);
+                }
+            });
+
+        });
+
+    }
+});
+
+Template.reportsSummary.onCreated(function () {
     this.subscribe( 'invoices.reports.total' );
     this.subscribe('appointments.day.patients');
 });
 
-Template.reportsContent.helpers({
+Template.reportsSummary.helpers({
     totalAmount: function () {
         var todaysDate = new RegExp( moment().format('Do MMM YYYY') );
         var todaysInvoices = Invoices.find( { date_issued: { $regex: todaysDate } }, { fields: { amount: 1 } }).fetch();
@@ -42,42 +79,5 @@ Template.reportsContent.helpers({
     monthsPatients: function () {
         var thisMonth = new RegExp( moment().format('MMM YYYY') );
         return Appointments.find( { date_created: { $regex: thisMonth } }, { fields: { date_created: 1 } }).count();
-    }
-});
-
-Template.reportsContent.events({
-    'click .reports-tabs': function () {
-        /*
-         Tab functionality
-         More details: https://codyhouse.co/gem/responsive-tabbed-navigation/
-         */
-
-        var tabs = $('.body-flesh');
-        tabs.each(function () {
-            var tab = $(this),
-                tabItems = tab.find('.reports-tabs'),
-                tabContentWrapper = tab.children('.reports-tabs-content');
-
-
-            tab.on('click', '.reports-tabs-link', function (event) {
-                event.preventDefault();
-                var selectedItem = $(this);
-
-                if ( !selectedItem.hasClass('reports-tabs-link-is-selected') ) {
-                    var selectedTab = selectedItem.data('content'),
-                        selectedContent = tabContentWrapper.find('li[data-content="'+ selectedTab +'"]'),
-                        selectedContentHeight = selectedContent.innerHeight();
-
-                    tabItems.find('.reports-tabs-link-is-selected').removeClass('reports-tabs-link-is-selected');
-                    selectedItem.addClass('reports-tabs-link-is-selected');
-                    selectedContent.addClass('reports-tabs-content-section-is-selected').siblings('li').removeClass('reports-tabs-content-section-is-selected');
-                    tabContentWrapper.animate({
-                        'height': selectedContentHeight
-                    }, 200);
-                }
-            });
-
-        });
-
     }
 });
